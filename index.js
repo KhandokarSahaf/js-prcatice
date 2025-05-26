4998cbd13222eb238c4a449b7cbd02216ec5fa62
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-05-26 20:41:07 +0600

    const subNam = [English, CyberSecurity, Math, Physics, Biology]; const myMarks = [85, 90, 50, 70, 80];
    console.log(`Subject : ${subNam[0]} Marks : ${myMarks[0]}`);
    console.log(`Subject : ${subNam[1]} Marks : ${myMarks[1]}`);
    console.log(`Subject : ${subNam[2]} Marks : ${myMarks[2]}`);
    console.log(`Subject : ${subNam[3]} Marks : ${myMarks[3]}`);
    console.log(`Subject : ${subNam[4]} Marks : ${myMarks[4]}`);

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// *****  FIRST PART *****//
 const num = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-
-console.log(num);
 console.log(num[3]);
 
 const fifth = num[4];
 console.log(fifth);
+// ***** SECOND PART *****//
+const subNam = ["English", "CyberSecurity", "Math", "Physics", "Biology"];
+const myMarks = [85, 90, 50, 70, 80];
+
+console.log(`Subject : ${subNam[0]} Marks : ${myMarks[0]}`);
+console.log(`Subject : ${subNam[1]} Marks : ${myMarks[1]}`);
+console.log(`Subject : ${subNam[2]} Marks : ${myMarks[2]}`);
+console.log(`Subject : ${subNam[3]} Marks : ${myMarks[3]}`);
+console.log(`Subject : ${subNam[4]} Marks : ${myMarks[4]}`);
