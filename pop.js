3bd87a55f4bca223d8dbdd9cb3a25fd430482434
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-05-26 09:32:32 +0600

    pop funtion testing

---
+++ pop.js
@@ -0,0 +1,17 @@
+const name = ['Sahaf', 'Dihan', 'Sadman', 'Sumon', 'Nafess']
+
+console.log(name);
+
+name.pop();
+
+console.log(name);
+
+name.push('Rohan');
+
+console.log(name);
+
+name.pop();
+name.pop();
+name.pop();
+
+console.log(name);
