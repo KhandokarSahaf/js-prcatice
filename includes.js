0da82e9d113c983b0d8f235adb311a1b126d8c9f
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-05-28 13:54:47 +0600

    indcludes.js a push pop use koire includes cheek korlam

--- includes.js
+++ includes.js
@@ -1,11 +1,20 @@
-const friendsName = ['sadman', 'sumon', 'siam', 'sayma', 'kormokar'];
-console.log(friendsName.includes('sumon'));
-// includes use koire veriable ar vitore kono specific index ke khuje ber kora hoy 
-console.log(friendsName.includes('sahaf'));
+const friendsName = ["sadman", "sumon", "siam", "sayma", "kormokar"];
+console.log(friendsName.includes("sumon"));
+// includes use koire veriable ar vitore kono specific index ke khuje ber kora hoy
+console.log(friendsName.includes("sahaf"));
 
-if(friendsName.includes('kormokar')){
-    console.log("tum ak behen ki lori ho bsrike");
+if (friendsName.includes("kormokar")) {
+  console.log("tum ak behen ki lori ho bsrike");
+} else {
+  console.log("oh,ok you can be my friend");
 }
-else{
-    console.log("oh,ok you can be my friend");
+
+let myFriends = ["html", "css", "tailwind", "disyUi"];
+myFriends.push("javascript");
+myFriends.pop("javascript");
+
+if (myFriends.includes("javascript")) {
+  console.log("good job tumi selected");
+} else {
+  console.log("duro tumi kisui paro nh");
 }
