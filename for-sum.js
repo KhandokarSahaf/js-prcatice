9f55d95c500f8dc089b2b5cdcb35efb24b41a6fa
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-03 14:34:17 +0600

    for sum a kisu change

--- for-sum.js
+++ for-sum.js
@@ -1,8 +1,18 @@
 // even Number
-for(let i = 2; i <= 10; i=i+i){
-    console.log(i);
+// for (let i = 2; i <= 10; i = i + i) {
+//   console.log(i);
+// }
+// // odd number
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log(i);
 }
-// odd number
-for(let i=1; i <=10; i=i+2){
-    console.log(i);
-}git 
+
+// sum 
+
+// let sum = 0;
+
+// for(let i = 1; i<=10; i++){
+//   sum = sum+i;
+//   console.log(i);
+//   console.log(`total sum ${sum}`);
+// }
